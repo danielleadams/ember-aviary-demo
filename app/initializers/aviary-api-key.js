72e0851b71008515047803417fb3188b21b064e6
Ken Sin <kensin1129@gmail.com>
2015-03-26 17:20:13 -0400

    Don't need to initialize. Just use dotenv

--- app/initializers/aviary-api-key.js
+++
@@ -1,9 +0,0 @@
-import ENV from 'aviary-demo/config/environment';
-
-export default {
-  name: 'aviary-api-key',
-  before: 'aviary-service',
-  initialize: function(container, application) {
-    window.AVIARY_API_KEY = ENV.AVIARY_API_KEY;
-  }
-};
